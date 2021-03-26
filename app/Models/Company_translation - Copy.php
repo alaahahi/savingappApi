b62afe131a84b7b8a9cfcbb15a5ac0a250f03acc
alaahahi <alaa.hahe@gmail.com>
2021-03-26 16:08:51 +0300

    -

--- app/Models/Company_translation - Copy.php
+++ app/Models/Company_translation - Copy.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category_translation extends Model
+class Company_translation extends Model
 {
-    protected $table = 'category_translation';
+    protected $table = 'company_translation';
     use HasFactory;
 }
