2e015737c4b51cdeb17489d0755eb01730a8cd1c
alaahahi <alaa.hahe@gmail.com>
2021-03-28 22:10:20 +0300

    Update CardTypeController.php

--- app/Http/Controllers/CardTypeController.php
+++ app/Http/Controllers/CardTypeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Card_type;
 use Orion\Http\Controllers\Controller;
 use Orion\Concerns\DisableAuthorization;
+use Orion\Concerns\DisablePagination;
 
 class CardTypeController extends Controller
 {
